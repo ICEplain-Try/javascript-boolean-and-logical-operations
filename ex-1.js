dff5379845c6a64249d748c504d19541bfcd9fa3
ICE K. <info.icejourney@gmail.com>
2024-08-10 18:06:04 +0700

    update : ex1-3

--- ex-1.js
+++ ex-1.js
@@ -5,4 +5,12 @@ let isAllow;
 
 // Start coding here
 
+// ใส่ค่าคุณสมบัติของคนชื่อ James จากโจทย์
+isOver18 = true;
+hasCriminalBlacklist = false;
+
+// Reassign Variable ที่ชื่อ isAllow
+isAllow = isOver18 && !hasCriminalBlacklist;
+
+
 console.log(isAllow);
--- ex-3.js
+++ ex-3.js
@@ -4,6 +4,9 @@
 const numberA = 100;
 const numberB = "20";
 
-numberA = 10;
+// numberA = 10; ต้องลบบรรนี้ออก เพราะ const Reassignment ไม่ได้ เลยทำเป็น Comment ไว้
 
-console.log(`Sum: ${numberA + numberB}`);
+
+// ส่วนนี้คือการแปลง numberA, numberB เป็นตัวเลข แล้วใช้ตัว Math Operator เล็กน้อยทำให้ผลลัพธ์ Sum: มีค่าเท่ากับ 30
+console.log(`Sum: ${Number(numberA / 10) + Number(numberB)}`); // Sum: 30
+
